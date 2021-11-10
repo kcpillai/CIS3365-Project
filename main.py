2f4817305b9a7e20ab750df9aa14828ae9b8b0a8
kcpillai <77808972+kcpillai@users.noreply.github.com>
2021-11-09 23:35:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,40 @@ import flask
 import hashlib
 from flask import jsonify
 from flask import request, make_response
+from tkinter import *
+from PIL import Image, ImageTk  # pip install pillow
+from tkinter import ttk
+import random
+from mysql import connector
+import pyodbc
+from tkinter import messagebox
+import mysql.connector
+from mysql.connector import Error
 from sql import create_connection
 from sql import execute_query
-from mysql.connector import Error
-import datetime
-from datetime import datetime
+from sql import execute_read_query
 
-# CREATE A STRING FOR THE CURRENT DATE
-# FOUND FROM ONLINE SOURCE - https://www.tutorialspoint.com/How-to-store-and-retrieve-a-date-into-MySQL-database-using-Python
-now = datetime.now()
-formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
+username = 'rdramir4'
+password = '#HoneyB33z'
+DB = 'cis3368pqs'
+Connect = "DRIVER={{SQL Server}};SERVER=COT-CIS3365-09.cougarnet.uh.edu;DATABASE={};UID={};PWD={}".format(
+    DB, username, password)
+print('Connect')
 
 
+app = flask.Flask(__name__)  # sets up the application
+app.config["DEBUG"] = True  # allow to show errors in browser
 
-app = flask.Flask(__name__) #  sets up the application
-app.config["DEBUG"] = True #  allow to show errors in browser
 
-# username is set as 'admin'
-# password from apisecurity class, password is set as 'password'
-masterPassword = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
+@app.route('/api/add', methods=['GET'])
+def test():
 
+    conn = pyodbc.connect(Connect)
+    print("Connection to MySQL DB successful")
+    my_cursor = conn.cursor()
+    all_logs = "SELECT * FROM gender"
+    my_cursor.execute = execute_read_query(Connect, all_logs)
+    return jsonify(my_cursor)
 
 
 @app.route('/test', methods=['POST']) 
